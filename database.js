7219806490724f449fa904fbaa06b00ffe9a65de
tmnkakr <tmnkakr@gmail.com>
2023-03-27 04:04:46 +0530

    comoit

---
+++ database.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require("mongodb");
+const url =
+  "mongodb+srv://user:bJX7jl0dIixuPJYT@marketingwebapp.vcafwfd.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function dbConnect() {
+  let result = await client.connect();
+  db = result.db("database");
+  return db.collection("saleBills");
+}
+
+module.exports = dbConnect;
--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dbConnect = require("./database");
+
+let port = process.env.port || 80;
 
 const corsOptions = {
   origin: "*",
-  credentials: true, //access-control-allow-credentials:true
+  credentials: true,
   optionSuccessStatus: 200,
 };
 
@@ -14,12 +17,23 @@ app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
-app.post("/data", (req, res) => {
+app.post("/data", async (req, res) => {
   const data = req.body;
-  console.log("Received data:", data);
+  try {
+    let conne = await dbConnect();
+    let result = await conne.insertOne(data);
+    if (result) {
+      console.log("Data Inserted");
+    } else console.log("Received Status Code other than 200");
+  } catch (e) {
+    console.error("Error ", e);
+  }
+  res.json({ message: "Data received" });
+});
+app.get("/data", async (req, res) => {
   res.json({ message: "Data received" });
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(port, () => {
+  console.log("Server is listening on port" + port);
 });
